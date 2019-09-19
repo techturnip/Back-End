eb4493ff610d7d227766ac0591be3e71246cef9f
Tyler Turnipseed <tyler@techturnip.us>
2019-09-19 19:55:01 -0400

    created and ran migrations for the database

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/dbname.db3'
+      filename: './data/journal.db3'
     },
     useNullAsDefault: true,
     migrations: {
