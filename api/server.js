800d61623738a0fba926419e3692c09b4215df8a
Tyler Turnipseed <tyler@techturnip.us>
2019-09-19 17:47:04 -0400

    bring in and setup routers in server.js

--- api/server.js
+++ api/server.js
@@ -4,17 +4,31 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 require('dotenv').config()
+// bring in routers -------------------------------|
+const authRouter = require('../auth/auth-router.js')
+const usersRouter = require('../users/user-router.js')
+const postsRouter = require('../posts/post-router.js')
+// create express server --------------------------|
 const server = express()
+// ------------------------------------------------|
 // GLOBAL MIDDLEWARES =============================|
 // ================================================|
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
+// ------------------------------------------------|
+// SETUP ROUTES ===================================|
+// ================================================|
+server.use('/api/auth/', authRouter)
+server.use('/api/users/', usersRouter)
+server.use('/api/posts/', postsRouter)
+// ------------------------------------------------|
 // ROOT ENDPOINT ==================================|
 // ================================================|
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'is running' })
 })
+// ------------------------------------------------|
 // EXPORT =========================================|
 // ================================================|
 module.exports = server
