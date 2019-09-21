788092dd6f7d4071f49fea14405de4d82b673d60
Tyler Turnipseed <tyler@techturnip.us>
2019-09-21 17:58:29 -0400

    refactored add method

--- users/user-model.js
+++ users/user-model.js
@@ -5,17 +5,17 @@ const db = require('../data/dbConfig.js')
 // DEFINE DB HELPERS ==============================|
 // ================================================|
 // Sep. 21 - Refactored for modularity ------------|
-const add = async user => {
-  const [id] = await db('users').insert(user)
-  return findById(id)
-}
-// const add = user => {
-//   db('users')
-//     .insert(user)
-//     .then(ids => {
-//       return findBy({ id: ids[0] })
-//     })
+// const add = async user => {
+//   const [id] = await db('users').insert(user)
+//   return findById(id)
 // }
+const add = user => {
+  db('users')
+    .insert(user)
+    .then(ids => {
+      return findBy({ id: ids[0] })
+    })
+}
 // ------------------------------------------------|
 // Sep. 21 - Refactored for modularity ------------|
 const find = () => db('users')
