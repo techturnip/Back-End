b5a2e96171701d6b60a6fa4ec86ec97c6c07e67c
Tyler Turnipseed <tyler@techturnip.us>
2019-09-21 15:11:15 -0400

    working on seeding db

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,13 +1,16 @@
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('users')
+    .del()
+    .then(() => {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+        { id: 1, colName: 'rowValue1' },
+        { id: 2, colName: 'rowValue2' },
+        { id: 3, colName: 'rowValue3' }
+      ])
+    })
+}
