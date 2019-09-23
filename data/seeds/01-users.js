ff8ab1754d9dc31cf03c16c2058023222da865ae
Tyler Turnipseed <tyler@techturnip.us>
2019-09-22 20:51:50 -0400

    added comments to the seeding functions

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const faker = require('faker')
 const knexCleaner = require('knex-cleaner')
 
+// set options for knex cleaner
 const options = {
   mode: 'truncate',
   restartIdentity: true,
@@ -9,14 +10,19 @@ const options = {
 }
 
 exports.seed = knex => {
-  // Deletes ALL existing entries
+  // cleans users table from the db
   return knexCleaner.clean(knex, options).then(() => {
+    // set empty array to store the generated users
     const users = []
+    // set the number of users to generate
     const numOfUsers = 25
 
+    // loop over the number of users
     for (let i = 0; i < numOfUsers; i++) {
+      // store password to be ran through bcrypt
       const password = faker.internet.password()
 
+      // each iteration will generate a user object
       const user = {
         fname: faker.name.firstName(),
         lname: faker.name.lastName(),
@@ -25,12 +31,17 @@ exports.seed = knex => {
         password: bcrypt.hashSync(password, 10)
       }
 
+      // console.log() the id, username, and password
+      // so that the generated users can be used
       console.log(
         `id: ${i + 1}\nusername: ${user.username}\npassword: '${password}'\n`
       )
+
+      // push the user to the array above
       users.push(user)
     }
 
+    // insert the array of users into the database
     return knex('users').insert(users)
   })
 }
